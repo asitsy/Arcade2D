524ef338cb63a8f2a739a8fbf88bc06fc8b74994
Anastasiia Tsyban <anasta.tsyban@gmail.com>
2026-05-15 01:07:18 +0200

    Change colors (background + player prototype)

--- Game1.cs
+++ Game1.cs
@@ -9,6 +9,9 @@ public class Game1 : Microsoft.Xna.Framework.Game
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
+    private Player _player;
+    private Texture2D _pixel;
+
     public Game1()
     {
         _graphics = new GraphicsDeviceManager(this);
@@ -21,16 +24,36 @@ public class Game1 : Microsoft.Xna.Framework.Game
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        _pixel = new Texture2D(GraphicsDevice, 1, 1);
+
+        _pixel.SetData(new[]
+        {
+            new Color(255, 182, 193)
+        });
+
+        _player = new Player(
+            new Vector2(300, 300),
+            _pixel
+        );
     }
 
     protected override void Update(GameTime gameTime)
     {
+        _player.Update(gameTime);
+
         base.Update(gameTime);
     }
 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.Black);
+        GraphicsDevice.Clear(new Color(18, 20, 38));
+
+        _spriteBatch.Begin();
+
+        _player.Draw(_spriteBatch);
+
+        _spriteBatch.End();
 
         base.Draw(gameTime);
     }

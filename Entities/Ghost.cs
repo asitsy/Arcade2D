72a93eb7e493a65f679e1d11b8a4886a0dfa6414
Anastasiia Tsyban <anasta.tsyban@gmail.com>
2026-06-07 11:37:43 +0200

    Improve ghost ai

--- Entities/Ghost.cs
+++ Entities/Ghost.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -6,7 +7,17 @@ namespace Arcade2D.Entities;
 
 public class Ghost : Entity
 {
-    private Vector2 _direction = Vector2.UnitX;
+    private readonly Random _random = new();
+
+    private Vector2 _direction = new Vector2(1, 0);
+
+    private readonly Vector2[] _directions =
+    {
+        new Vector2(0, -1),
+        new Vector2(0, 1),
+        new Vector2(-1, 0),
+        new Vector2(1, 0)
+    };
 
     public Ghost(
         Vector2 position,
@@ -57,11 +68,55 @@ public class Ghost : Entity
 
         if (collision)
         {
-            _direction *= -1;
+            ChangeDirection(walls);
         }
         else
         {
             Position = nextPosition;
+        }
+    }
+
+    private void ChangeDirection(List<Wall> walls)
+    {
+        List<Vector2> availableDirections = new();
+
+        foreach (Vector2 direction in _directions)
+        {
+            Vector2 testPosition =
+                Position +
+                direction * 32;
+
+            Rectangle testBounds =
+                new(
+                    (int)testPosition.X,
+                    (int)testPosition.Y,
+                    Bounds.Width,
+                    Bounds.Height
+                );
+
+            bool blocked = false;
+
+            foreach (Wall wall in walls)
+            {
+                if (testBounds.Intersects(wall.Bounds))
+                {
+                    blocked = true;
+                    break;
+                }
+            }
+
+            if (!blocked)
+            {
+                availableDirections.Add(direction);
+            }
+        }
+
+        if (availableDirections.Count > 0)
+        {
+            _direction =
+                availableDirections[
+                    _random.Next(availableDirections.Count)
+                ];
         }
     }
 
